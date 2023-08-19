051b0729367c96c667dba050eef69a12f449f962
taufik-hdyt <taufikhdyt2599@gmail.com>
2023-08-19 11:43:16 +0700

    task 8

--- js/oopJS/1.class.js
+++ js/oopJS/1.class.js
@@ -1,10 +1,24 @@
-class Car {
-  contructor(make, model) {
-    this.make = make;
-    this.model = model;
-  }
+// class Car {
+//   contructor(make, model) {
+//     this.make = make;
+//     this.model = model;
+//   }
 
-  getInfo() {
-    return `This Car ${this.make} Model${this.model}`;
+//   getInfo() {
+//     return `This Car ${this.make} Model${this.model}`;
+//   }
+// }
+
+// =====
+class Car {
+  name = "Odong odong";
+  color = "Black kadut";
+  constructor(merek) {
+    this.name = merek;
+  }
+  melaju() {
+    return `This is a ${this.name}`;
   }
 }
+const mobil = new Car("Truck");
+console.log(mobil.melaju());
